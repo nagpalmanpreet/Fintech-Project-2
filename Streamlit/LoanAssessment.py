2f7a5ad03e6a126e10efb024c1ead881b1579d48
Manpreet Singh <nagpalmanpreet@gmail.com>
2022-09-04 00:19:45 +1000

    Added .jpeg logo file

--- Streamlit/LoanAssessment.py
+++ Streamlit/LoanAssessment.py
@@ -12,7 +12,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.write(' ')
 with col2:
-    st.image('./Images/LoanApproved.png')
+    st.image('./Images/LoanApproved.jpeg')
 with col3:
     st.write(' ')
 
